eaf2fb044cc6e52404f7c35b9ecb7de9e7b4d4c2
saathvika <sdathrika@nexient.com>
2023-11-10 13:58:40 -0500

    remove pre commit config yaml file

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 terraform {
-  required_version = "<= 1.5.5"
+  required_version = ">= 1.5.5"
 
   required_providers {
     azurerm = {
